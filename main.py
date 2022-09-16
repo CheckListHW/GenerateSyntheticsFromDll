65bbdad573d54efd85e9d2c458c667b99190c487
Иван Косачев <kosach_1@live.com>
2022-09-16 14:23:27 +0700

    add time check

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from utils.synthetic_generator.generator import SyntheticGenerator
 
 if __name__ == '__main__':
     scenario_path = os.getcwd() + '/input.xml'
-    generator = SyntheticGenerator(scenario_path)
+    generator = SyntheticGenerator(scenario_path, 120, 25)
 
     top_x1 = 966.7529628351272
     top_y1 = 1921.40443483379
@@ -19,20 +19,26 @@ if __name__ == '__main__':
 
     x, y = [], []
     y_step = 0
+    x_step = 0
     step = 30
     for increment in [0, 1]:
+        import time
+        start_time = time.time()
+        print(f'start: {time.time() - start_time}')
         curves = generator.generate(top_x1 + step * increment, top_y1,
                                     top_x2 + step * increment, top_y2,
                                     bot_x1 + step * increment, bot_y1,
                                     bot_x2 + step * increment, bot_y2)
+        print(f'finish: {round(time.time() - start_time, 4)}s synthetics count: {len(curves)}')
 
         for curve in curves:
             for point in curve.Curve.Points:
-                x.append(point.Position)
+                x.append(point.Position + x_step)
                 y.append(point.Value + y_step)
             y_step += 15
-
             plt.plot(x, y)
             x, y = [], []
-        y_step += 60
+
+        x_step += 3000
+        y_step = 0
     plt.show()
