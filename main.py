a030ff46819f8d15ca65ecb757745c954a750874
Иван Косачев <kosach_1@live.com>
2022-09-19 11:12:32 +0700

    change generator.py

--- main.py
+++ main.py
@@ -1,44 +1,29 @@
 import os
 
-from matplotlib import pyplot as plt
-
 from utils.synthetic_generator.generator import SyntheticGenerator
+
+from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     scenario_path = os.getcwd() + '/input.xml'
-    generator = SyntheticGenerator(scenario_path, 120, 25)
+    generator = SyntheticGenerator(scenario_path)
 
-    top_x1 = 966.7529628351272
-    top_y1 = 1921.40443483379
-    top_x2 = 996.7529628351272
-    top_y2 = 1926.6005872564963
-    bot_x1 = 966.7529628351272
-    bot_y1 = 1926.805672095235
-    bot_x2 = 996.7529628351272
-    bot_y2 = 1932.0018245179413
+    top_x = [966.7529628351272, 996.7529628351272]
+    top_y = [1921.40443483379, 1926.6005872564963]
+    bot_x = [966.7529628351272, 996.7529628351272]
+    bot_y = [1926.805672095235, 1932.0018245179413]
 
     x, y = [], []
-    y_step = 0
-    x_step = 0
     step = 30
-    for increment in [0, 1]:
-        import time
-        start_time = time.time()
-        print(f'start: {time.time() - start_time}')
-        curves = generator.generate(top_x1 + step * increment, top_y1,
-                                    top_x2 + step * increment, top_y2,
-                                    bot_x1 + step * increment, bot_y1,
-                                    bot_x2 + step * increment, bot_y2)
-        print(f'finish: {round(time.time() - start_time, 4)}s synthetics count: {len(curves)}')
 
-        for curve in curves:
-            for point in curve.Curve.Points:
-                x.append(point.Position + x_step)
-                y.append(point.Value + y_step)
-            y_step += 15
-            plt.plot(x, y)
-            x, y = [], []
+    import time
+    start_time = time.time()
+    print(f'start: {time.time() - start_time}')
+    curve = generator.generate(top_x, top_y, bot_x, bot_y).Points
+    print(f'finish: {round(time.time() - start_time, 8)/1000}ms')
 
-        x_step += 3000
-        y_step = 0
+    for point in curve:
+        x.append(point.Position)
+        y.append(point.Value)
+    plt.plot(x, y)
     plt.show()
--- utils/synthetic_generator/generator.py
+++ utils/synthetic_generator/generator.py
@@ -1,23 +1,12 @@
+from typing import List
+
 from GeosteeringOffice.Autocorrelation.Api import Transfer
-from AutoCorrelation import GenerateSynthetics
+from AutoCorrelation import GenerateSyntheticsFromSurface
 
 
 class SyntheticGenerator:
-    def __init__(self, scenario_path, angle: int = 120, synthetic_count: int = 50):
-        self.Scenario = Transfer.Load(scenario_path)
-        self.Generator = GenerateSynthetics()
-        self.Generator.Angle = angle
-        self.Generator.SyntheticCountOnStep = synthetic_count
+    def __init__(self, scenario_path):
+        self.Generator = GenerateSyntheticsFromSurface(Transfer.Load(scenario_path))
 
-    def generate(self, top_x1, top_y1, top_x2, top_y2, bot_x1, bot_y1, bot_x2, bot_y2):
-        self.Generator.FirstPointTopX = top_x1
-        self.Generator.FirstPointTopY = top_y1
-        self.Generator.SecondPointTopX = top_x2
-        self.Generator.SecondPointTopY = top_y2
-
-        self.Generator.FirstPointBottomX = bot_x1
-        self.Generator.FirstPointBottomY = bot_y1
-        self.Generator.SecondPointBottomX = bot_x2
-        self.Generator.SecondPointBottomY = bot_y2
-
-        return self.Generator.Generate(self.Scenario)
+    def generate(self, top_x: List[float], top_y: List[float], bot_x: List[float], bot_y: List[float]):
+        return self.Generator.GenerateSyntheticsFromList(top_x, top_y, bot_x, bot_y)
